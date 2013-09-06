2a3ea3ef9124af3e45dd24ea70664a2ce8aeb3d8
David Peake <dave.peake@gmail.com>
2013-09-06 10:49:56 +1000

    Fixed SSRL

--- ssrl.py
+++ ssrl.py
@@ -3,7 +3,10 @@
 Return the current at SLS
 '''
 
-import urllib
+import re
+
+import requests
+from bs4 import BeautifulSoup
 
 name = "Stanford Synchrotron Radiation Lightsource"
 country = "Menlo Park, USA"
@@ -12,12 +15,17 @@ isocode = 'US-CA'
 
 SSRL_URL = 'http://www-ssrl.slac.stanford.edu/talk_display.html'
 
-def get_current():
-	A = urllib.urlopen(SSRL_URL).readlines()
 
-	I = A[138].rstrip().rstrip(' mA</td>').lstrip('  <td class="right">Beam Current: ').lstrip()
- 
-	return eval(I)
-	
+def get_current():
+    r = requests.get(SSRL_URL)
+    soup = BeautifulSoup(r.text)
+
+    # gives Beam Current: XX mA
+    webtxt = soup.findAll('td', {'class': 'left'})[0].contents[4].strip()
+
+    current_txt = re.search(r"[-+]?(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?", webtxt)
+
+    return float(current_txt.group(0))
+
 if __name__ == '__main__':
-	print get_current()
+    print get_current()
