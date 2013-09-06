5c7c6974221d470a06b2a2719aa16e06d03f1795
David Peake <dave.peake@gmail.com>
2013-09-07 09:42:09 +1000

    Fixed APS

--- aps.py
+++ aps.py
@@ -3,7 +3,8 @@
 Return the current at APS
 '''
 
-import urllib, os
+import urllib
+import os
 from PIL import Image, ImageFile
 import numpy
 
@@ -16,35 +17,35 @@ APS_URL = 'http://www.aps.anl.gov/aod/blops/plots/smallStatusPlot.png'
 
 offset = '::10'
 
+
 def get_current():
-	gif = urllib.urlopen(APS_URL)
-	#gif = 'images/nsls_ringstat.gif'
+    gif = urllib.urlopen(APS_URL)
+    #gif = 'images/nsls_ringstat.gif'
 
-	parser = ImageFile.Parser()
-	while True:
-		s = gif.read(1024)
-		if not s:
-			break
-		parser.feed(s)
+    parser = ImageFile.Parser()
+    while True:
+        s = gif.read(1024)
+        if not s:
+            break
+        parser.feed(s)
 
-	A = parser.close()	
-	#A = PIL.Image.open(gif)
-	B = A.crop([187,18,240,38])
+    A = parser.close()
+    #A = PIL.Image.open(gif)
+    B = A.crop([187, 18, 240, 38])
 
-	Ba = numpy.asarray(B)
+    Ba = numpy.asarray(B)
 
+    B.save('temp.tif')
+    #B.show()
 
-	B.save('temp.tif')
-	B.show()
+    os.popen('tesseract temp.tif temp 2> /dev/null')
 
-	os.popen('tesseract temp.tif temp 2> /dev/null')
+    #check if temp.txt exists
+    I = eval(open('temp.txt').readline().rstrip())
 
-	#check if temp.txt exists	
-	I = eval(open('temp.txt').readline().rstrip())
+    os.system('rm temp.tif temp.txt')
 
-	os.system('rm temp.tif temp.txt')
-
-	return I
+    return I
 
 if __name__ == '__main__':
-	print get_current()
+    print get_current()
